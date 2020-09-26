649b2d41b58723cf1ab70d09649f9af2081bfb96
Jessamin Karabinovich <64698383+MashaKarabinovich@users.noreply.github.com>
2020-09-26 11:17:30 -0400

    Add files via upload

---
+++ connection.php
@@ -0,0 +1,16 @@
+<?php
+// PHP Data Objects(PDO) Sample Code:
+try {
+    $conn = new PDO("sqlsrv:server = tcp:webmasha1.database.windows.net,1433; Database = webmasha", "Masha123", "{your_password_here}");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
+
+// SQL Server Extension Sample Code:
+$connectionInfo = array("UID" => "Masha123", "pwd" => "{your_password_here}", "Database" => "webmasha", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:webmasha1.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
+?>
---
+++ login.php
@@ -0,0 +1,24 @@
+<?php
+
+$nameVariable = $_GET ["userID"];
+$_GET ["gradesEntered"];
+//$_POST ["gradesEntered"]
+
+
+?>
+
+
+
+<html>
+   <head>
+        <title>Login</title>
+      <body>
+
+<?php
+
+echo "<b>$nameVariable</b";
+
+?>
+
+</body>
+</html>
