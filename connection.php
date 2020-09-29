6bacc46bfe7a8c0a3c85fe3a4d21ff06c29eb76c
Jessamin Karabinovich <64698383+MashaKarabinovich@users.noreply.github.com>
2020-09-29 12:46:20 -0400

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,16 +1,10 @@
 <?php
-// PHP Data Objects(PDO) Sample Code:
-try {
-    $conn = new PDO("sqlsrv:server = tcp:webmasha1.database.windows.net,1433; Database = webmasha", "Masha123", "Hidden23");
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-catch (PDOException $e) {
-    print("Error connecting to SQL Server.");
-    die(print_r($e));
-}
 
-// SQL Server Extension Sample Code:
-$connectionInfo = array("UID" => "Masha123", "pwd" => "{your_password_here}", "Database" => "webmasha", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
-$serverName = "tcp:webmasha1.database.windows.net,1433";
-$conn = sqlsrv_connect($serverName, $connectionInfo);
+$host = webmasha1.database.windows.net;
+$username= 'Masha123';
+$password = 'Hidden23';
+$db_name = 'webmasha1';
+$conn = mysqli_init();
+mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306);
+
 ?>
